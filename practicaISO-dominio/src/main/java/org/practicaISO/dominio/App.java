b5ed86c2cbc44c87181344eb91c5fe6de47ee8b5
alber <Alberto.Vinaroz@alu.uclm.com>
2017-11-24 13:12:29 +0100

    nuevo

---
+++ practicaISO-dominio/src/main/java/org/practicaISO/dominio/App.java
@@ -0,0 +1,13 @@
+package org.practicaISO.dominio;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ practicaISO-presentacion/src/main/java/org/practicaISO/presentacion/App.java
@@ -0,0 +1,13 @@
+package org.practicaISO.presentacion;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
