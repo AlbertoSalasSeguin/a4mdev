9a2a1e64957f215d660d8b6ccc2e286de17721c7
Fernando <Fernando@192.168.1.33>
2017-12-28 20:53:37 +0100

    nuevo

---
+++ practicaISO-dominio/src/test/java/org/practicaISO/dominioTEST/AlbumTest.java
@@ -0,0 +1,29 @@
+package org.practicaISO.dominioTEST;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AlbumTest {
+
+	@Test
+	public void testObtenerAlbum() {
+	}
+
+	@Test
+	public void testObtenerIdsCancionesAlbum() {
+	}
+
+	@Test
+	public void testInsertarAlbum() {
+	}
+
+	@Test
+	public void testActualizarAlbum() {
+	}
+
+	@Test
+	public void testEliminarAlbum() {
+	}
+
+}
---
+++ practicaISO-dominio/src/test/java/org/practicaISO/dominioTEST/ClienteTest.java
@@ -0,0 +1,37 @@
+package org.practicaISO.dominioTEST;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ClienteTest {
+
+	@Test
+	public void testLogearCliente() {
+	}
+
+	@Test
+	public void testObtenerCliente() {
+	}
+
+	@Test
+	public void testObtenerCanciones() {
+	}
+
+	@Test
+	public void testInsertarCliente() {
+	}
+
+	@Test
+	public void testActualizarCliente() {
+	}
+
+	@Test
+	public void testComprarCancion() {
+	}
+
+	@Test
+	public void testEliminarCliente() {
+	}
+
+}
